c7aa37cc83f301b2e84952c65e31645209d309f6
AdityaJha2901 <92071927+AdityaJha2901@users.noreply.github.com>
2024-09-12 15:29:11 +0530

    Time: 3 ms (61.37%) | Memory: 53.1 MB (41.27%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -5,26 +5,26 @@ class Solution {
         //counter
         int c=1;
         // current element
-        int curr = nums[0];
+        int curr = nums[nums.length/2];
         
-        //logic
-        for(int i=1; i<nums.length; i++){
+        // //logic
+        // for(int i=1; i<nums.length; i++){
 
-            //checking
-            if(nums[i]==curr){
-                c++;
-            }
+        //     //checking
+        //     if(nums[i]==curr){
+        //         c++;
+        //     }
             
-            else if(nums[i]!= curr && c>nums.length/2){
-                break;
-            }
-            else {
-                c=1;
-                curr= nums[i];
-            }
+        //     else if(nums[i]!= curr && c>nums.length/2){
+        //         break;
+        //     }
+        //     else {
+        //         c=1;
+        //         curr= nums[i];
+        //     }
 
             
-        }
+        // }
         return curr;
     } 
 }
